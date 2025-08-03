5e08c387cbcb36988a0131716232e611859ddb0c
SPluzh <131027290+SPluzh@users.noreply.github.com>
2025-08-03 23:48:08 +0500

    Add files via upload

---
+++ maya_install_pip_numpy2_local.cmd
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+
+REM Define the directory where the script is located
+set "SCRIPT_DIR=%~dp0"
+
+REM Specify the path to Python located in the same folder as the script
+set "MAYA_PYTHON_PATH=%SCRIPT_DIR%mayapy.exe"
+
+REM Check if Python exists
+if not exist "%MAYA_PYTHON_PATH%" (
+    echo Python not found in this folder. Please place this script next to the required mayapy.exe.
+    pause
+    exit /b 1
+)
+
+REM Install pip using get-pip.py
+echo Installing pip...
+"%MAYA_PYTHON_PATH%" "%SCRIPT_DIR%get-pip2.py"
+
+REM Install numpy
+echo Installing numpy...
+"%MAYA_PYTHON_PATH%" -m pip install numpy
+
+echo Installation completed.
+pause
